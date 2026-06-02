09e181aae1b2295b26cf1718a7bfd207e9870869
Yeri <mail@yeripratama.com>
2026-06-02 20:40:32 +0700

    L5 channel refactor

--- main.go
+++ main.go
@@ -7,26 +7,44 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+
+		line := ""
+		b := make([]byte, 8)
+		for {
+			n, err := f.Read(b)
+			if err == io.EOF {
+				break
+			}
+
+			line += string(b[:n])
+			parts := strings.Split(line, "\n")
+			for _, p := range parts[:len(parts)-1] {
+				ch <- p
+			}
+
+			line = parts[len(parts)-1]
+		}
+	}()
+
+	return ch
+}
+
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
-		fmt.Printf("Failed to open file\n")
-		return
+		fmt.Fprintf(os.Stderr, "Failed to open file: %v\n", err)
+		os.Exit(1)
 	}
-	line := ""
-	b := make([]byte, 8)
-	for {
-		n, err := f.Read(b)
-		if err == io.EOF {
-			break
-		}
+	defer f.Close()
 
-		line += string(b[:n])
-		parts := strings.Split(line, "\n")
-		for _, p := range parts[:len(parts)-1] {
-			fmt.Printf("read: %s\n", p)
-		}
+	ch := getLinesChannel(f)
 
-		line = parts[len(parts)-1]
+	for v := range ch {
+		fmt.Printf("read: %s\n", v)
 	}
 }
